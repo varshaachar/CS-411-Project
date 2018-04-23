f06a551dcee8443d1a589ef6f885fe4df8232988
varshaachar <varshaa@bu.edu>
2018-04-23 13:23:36 -0400

    added database stuff

--- routes/index.js
+++ routes/index.js
@@ -5,69 +5,174 @@ Payton Hauck
 Agustin Vasquez
 Lingwen Kong
 
-This takes a search keyword and a location, to return results from an API call to Yelp.
-
-Linked with index.pug
  */
 
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+
 router = express.Router();
 app = express();
-
+// TODO - ADD TO CONFIG FILE
+const api_key = 'bfo0qpJ7RAoVa_CMt-TYVTG1pNFWaEE_t2QQYn7PGAdFM03USgsyBWm_2huuFnGExakrN1ag4v-TYdAmn-ywmDXp9tIS9e8uTmRlm_dKjkgauxqikWusn-vy9se6WnYx';
 const yelp = require('yelp-fusion');
 const client = yelp.client(api_key);
 
+// empty at first
+var gymResult = {
+    name: "",
+    rating: "",
+    address: "",
+    phone: ""
+};
+
+// empty at first
+var yogaResult = {
+    name: "",
+    rating: "",
+    address: "",
+    phone: ""
+};
+
+
+const OpenWeatherMapHelper = require("openweathermap-node");
+const helper = new OpenWeatherMapHelper(
+    {
+        // TODO - ADD TO CONFIG FILE
+        APPID: '067147b8ff7bee8436e1ffb9ec383a2b',
+        units: "imperial"
+    }
+);
+
 /* GET home page. */
 app.get('/', function (req, res) {
-    res.render('fitness');
-})
-;
+    res.render('index');
+});
 
-app.post('/', function (req, res) {
-    var input_keyword = req.body.term;
-    var location = req.body.location;
+// GET about us page
+app.get('/about', function (req, res) {
+    res.render('about');
+});
 
-    console.log(req.body)
-    var searchRequest = {
-        term: input_keyword,
-        location: location
+// GET homepage
+app.get('/homepage', function (req, res) {
 
-    };
+    var user_city = "Boston";
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("fitbit");
 
-    // empty at first
-    var firstResult = {
-        name: "",
-        rating: "",
-        address: "",
-        phone: ""
-    };
-    console.log(searchRequest);  // logging for purposes of debugging
-    // noinspection JSAnnotator
-    client.search(searchRequest).then(response => {
+        // find a city
+        dbo.collection("weather").findOne({city: user_city}, function(err, result) {
+            if (err) throw err;
+            // if city doesn't exist -- make API call and add to database
+            if (result == null) {
+                helper.getCurrentWeatherByCityName(user_city, (err, currentWeather) => {
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        console.log(currentWeather)
+                        console.log(currentWeather.weather[0].description); //gives description of weather
+                        console.log(currentWeather.main.temp);      //gives temp
+                        var to_insert = { city: user_city, weather: currentWeather.main.temp, description: currentWeather.weather[0].description };
+                        // add it to the db
+                        dbo.collection("weather").insertOne(to_insert, function(err, res) {
+                        if (err) throw err;
+                        console.log("1 document inserted");
+                        });
+                    }
+            });
+            }
 
-        r = response.jsonBody.businesses[0];
-
-        console.log(r);
-        firstResult.name = r.name;
-        firstResult.address = r.location.display_address;
-        firstResult.phone = r.display_phone;
-        firstResult.rating = r.rating;
-        const prettyJson = JSON.stringify(firstResult, null, 4);
-
-        res.render('fitness', {
-            term: searchRequest.term,
-            location: searchRequest.location,
-            name: firstResult.name,
-            rating: firstResult.rating,
-            address: firstResult.address,
-            phone: firstResult.phone
         });
+        // now, retrieving from database to render it on the front end
+        dbo.collection("weather").findOne({city: user_city}, function(err, result) {
+            if (err) throw err;
+            db.close();
+            res.render('homepage', {temp: result.weather, desc: result.description});
+  });
+
 
     });
+//
+});
 
-    // logging the first result (empty)
-    console.log("First result", firstResult);
+/* Fitness page */
+app.get('/fitness', function (req, res) {
 
+    // GYMS!
+    // Search request for gyms - includes keyword 'gym' and location 'boston' (for now)
+    var gymSearchRequest = {
+        term: 'gym',
+        location: 'boston'
+    };
+
+
+    // noinspection JSAnnotator
+    client.search(gymSearchRequest).then(response => {
+
+        g = response.jsonBody.businesses[0];
+
+    console.log(g);  // logging the search results
+    gymResult.name = g.name;
+    gymResult.address = g.location.display_address;
+    gymResult.phone = g.display_phone;
+    gymResult.rating = g.rating;
+
+
+});
+
+    // YOGA!
+    // Search request for yoga - includes keyword 'yoga' and location 'boston' (for now)
+    var yogaSearchRequest = {
+        term: 'yoga',
+        location: 'boston'
+    };
+
+
+    // noinspection JSAnnotator
+    client.search(yogaSearchRequest).then(response => {
+
+        y = response.jsonBody.businesses[0];
+
+    console.log(y);  // logging the search results
+    yogaResult.name = y.name;
+    yogaResult.address = y.location.display_address;
+    yogaResult.phone = y.display_phone;
+    yogaResult.rating = y.rating;
+
+    res.render('fitness', {
+        g_name: gymResult.name,
+        g_rating: gymResult.rating,
+        g_address: gymResult.address,
+        g_phone: gymResult.phone, y_name: yogaResult.name,
+        y_rating: yogaResult.rating,
+        y_address: yogaResult.address,
+        y_phone: yogaResult.phone
+    });
+
+
+});
+
+});
+
+// GET food page
+app.get('/food', function (req, res) {
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("fitbit");
+
+        dbo.collection("recipes").findOne({}, function(err, result) {
+            if (err) throw err;
+            db.close();
+
+            res.render('food', {name: result.name, ingredients: result.ingredients, directions: result.directions});
+
+        });
+    });
 });
 
 module.exports = app;
